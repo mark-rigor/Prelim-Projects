2bf412430406e1c34e03838c571a5366b57cfafd
IMAC@Barrington <imacatbarrington@IMACBarringtons-iMac.local>
2025-11-24 22:10:11 -0500

    Add print statement for testing

--- test.py
+++ test.py
@@ -49,3 +49,4 @@ rounded_dailyAverageReturns = round(Average_Return2, 3)
 
 print(f"The stock price for {Stockname_str} every day of the week was {stock_data[Stockname_str]} ")
 print("The average return for the week is: " + str(rounded_dailyAverageReturns))
+print("change numero cinco brochacho this git commit is not working")
